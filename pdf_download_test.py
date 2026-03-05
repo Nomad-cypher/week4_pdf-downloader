1f94f8bd7f1736c6d1fb956db3cd14e157c46bbb
Nomad-cypher <258812503+Nomad-cypher@users.noreply.github.com>
2026-03-05 11:13:24 +0100

    Gave up on unit testing This is how far I got before I gave up on unit testing. Both functions use a try except structure, which I'm not sure how to test, since the functions already catch any potential errors. There's also the fact that both functions are VERY reliant on calls that are dependent on other components, so isolating them is very difficult. Any way, here are some posts I found on stackoverflow that convinced me it was best to drop it rather than waste more time: https://stackoverflow.com/questions/59862489/writing-a-unit-test-for-a-function-that-verifies-if-a-file-is-pdf-or-not https://stackoverflow.com/questions/57978703/python-unit-test-for-try-except-block

--- pdf_download_test.py
+++ pdf_download_test.py
@@ -1,2 +1,6 @@
-import unittest # Test module
+import unittest # Import test module
+from unittest.mock import patch, Mock # Import mocking functions
 from pdf_download import * # Import all functions and variables
+
+class test_validator(unittest.TestCase):
+    
